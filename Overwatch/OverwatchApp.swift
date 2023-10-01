9821636fc94284aafdf04f45268d8da662df7eec
Dmitry <d.myachn@gmail.com>
2023-10-01 13:10:26 +0300

    Initial Commit

---
+++ Overwatch/OverwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  OverwatchApp.swift
+//  Overwatch
+//
+//  Created by Dmitiy Myachin on 01.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct OverwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
